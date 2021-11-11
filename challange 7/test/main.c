003b54d7846f3433cc63a391e866e9eefff6d6ff
fatimazahra <fatimazahrabouamoud@gmail.com>
2021-11-11 14:45:36 +0100

    finale projet

---
+++ challange 7/test/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+
+{
+
+
+int a,b;
+ int  som , sos , mul, div , mod  ;
+
+printf("entre a : ");
+scanf ("%d",&a);
+printf("entre b: ");
+scanf ("%d",&b);
+ som = a+b;
+ mul = a*b;
+ div = a/b;
+ mod = a%b;
+ sos = a-b;
+
+     printf(" som %d , sostraction %d,division %d,multiplication %d,mod %d",som,sos,div,mod,sos );
+
+
+
+    return 0;
+}
