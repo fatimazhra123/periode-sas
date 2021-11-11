3f3b37ebf6b3707fb371a3bf9c76e8484e7e5d8a
fatimazahra <fatimazahrabouamoud@gmail.com>
2021-11-11 15:16:39 +0100

    projet finale

---
+++ challange 8/test/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+ int nomber1 = 1;
+ int nomber2 = 2 ;
+ int nomber3 = 3 ;
+ int nomber4 = 4 ;
+
+       int somme ;
+       double Moyenne ;
+	    somme = nomber1 + nomber2 + nomber3 + nomber4 ;
+	    Moyenne = somme / 4 ;
+
+
+     printf (" la somme est %d  et Moyenne est %f " , somme , Moyenne);
+
+
+    return 0;
+}
